87d4fabf59d44a9546b9b3651777a4152a6c9f59
- manas <rexboy3825@gmail.com>
2024-11-09 23:18:34 +0530

    Some minor changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        
+
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
